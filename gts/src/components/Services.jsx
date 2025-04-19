eac20a43c11bc22a2c5e158950ec4b5c92bb5330
ACER <210220@softwarica.edu.np>
2025-04-20 01:09:21 +0545

    comnt

--- gts/src/components/Services.jsx
+++ gts/src/components/Services.jsx
@@ -2,52 +2,54 @@ import React from 'react';
 
 const Services = () => {
 return (
-    <div className="Section3 h-auto w-full p-2" data-aos="fade-up">
-    <div className="servicetext">
-        <h1 className='font-titleFont text-xl lg:text-4xl text-center mt-4 text-appleblack'>OUR SERVICES</h1>
-    </div>
-    <div className="cards mt-10 grid grid-cols-1 lg:grid-cols-3 justify-center justify-items-center gap-y-10 mx-35">
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5'>Learn more</button>
-        </div>
+    <>
+    </>
+//     <div className="Section3 h-auto w-full p-2" data-aos="fade-up">
+//     <div className="servicetext">
+//         <h1 className='font-titleFont text-xl lg:text-4xl text-center mt-4 text-appleblack'>OUR SERVICES</h1>
+//     </div>
+//     <div className="cards mt-10 grid grid-cols-1 lg:grid-cols-3 justify-center justify-items-center gap-y-10 mx-35">
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5'>Learn more</button>
+//         </div>
 
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
-        </div>
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
+//         </div>
 
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
-        </div>
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
+//         </div>
 
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
-        </div>
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
-        </div>
-        <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
-        <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
-        <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
-        <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
-        <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
-        </div>
-    </div>
-</div>
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
+//         </div>
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
+//         </div>
+//         <div className="card1 rounded-xl h-auto w-[300px] lg:w-[400px] bg-white p-2">
+//         <img src='../images/server1.jpg' className=' rounded-xl' alt='card1'/>
+//         <h1 className='font-sans text-lg lg:text-xl text-center mt-2 text-appleblack'>Custom Server Setup</h1>
+//         <p className='font-sans text-sm lg:text-lg text-start mt-2 ml-3 text-appleblack'>Setting up of own and custom made servers with professionals.</p>
+//         <button className='text-center text-sm  lg:text-lg bg-blue-400 rounded-lg lg:rounded-xl p-2 mt-5 '>Learn more</button>
+//         </div>
+//     </div>
+// </div>
 )
 }
 
