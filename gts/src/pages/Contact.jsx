cff8acae99db392171ce431224f8f3d528518d75
ACER <210220@softwarica.edu.np>
2025-04-20 01:07:45 +0545

    contact mobile responsive

--- gts/src/pages/Contact.jsx
+++ gts/src/pages/Contact.jsx
@@ -20,7 +20,7 @@ alert("Credentials Submitted");
 return (
     <div className='Main-containerContact'>
     <Nav/>
-    <div className="contactPart h-[110vh] lg:h-[89vh] p-5  flex flex-col lg:flex-row justify-evenly items-center gap-10">
+    <div className="contactPart h-auto lg:h-[89vh] p-5  flex flex-col lg:flex-row justify-evenly items-center gap-10">
         <div className="Left">
         <h1 className="text-3xl font-bold text-appleblack">Contact Us</h1>
         <p className="text-appleblack text-lg">+977-9812345670 / 01-4312345</p>
@@ -29,24 +29,25 @@ return (
         <p className="text-appleblack text-lg">gts@gmail.com</p>
         </div>
 
-        <div className="Right h-[500px] w-[400px] lg:w-[500px] bg-slate-400 rounded-xl p-5 flex flex-col justify-center items-center gap-10">
+        <div className="Right h-auto w-[350px] lg:w-[500px] bg-slate-400 rounded-xl p-5 flex flex-col justify-center items-center gap-10">
         <input onChange={(e)=>setFullName(e.target.value)}
         value={fullName}
-        className='h-[50px] w-[90%] rounded-xl p-4 border-2 border-slate-600' placeholder='Full Name'></input>
+        className='h-[50px] w-[90%] mt-5 rounded-xl p-4 border-2 border-slate-600 text-sm lg:text-lg' placeholder='Full Name'></input>
         
         <input onChange={(e)=>setEmail(e.target.value)}
         value={email}
-        className='h-[50px] w-[90%] rounded-xl p-4 border-2 border-slate-600' placeholder='Email'></input>
+        className='h-[50px] w-[90%] rounded-xl p-4 border-2 border-slate-600 text-sm lg:text-lg' placeholder='Email'></input>
         
         <input onChange={(e)=>setPhNum(e.target.value)}
         value={phnum}
-        className='h-[50px] w-[90%] rounded-xl p-4 border-2 border-slate-600' placeholder='Phone No.'></input>
+        className='h-[50px] w-[90%] rounded-xl p-4 border-2 border-slate-600 text-sm lg:text-lg' placeholder='Phone No.'></input>
         
         <input onChange={(e)=>setDesc(e.target.value)}
         value={desc}
-        className='h-[90px] w-[90%] rounded-xl p-4 border-2 border-slate-600' placeholder='Description'></input>
+        className='h-[90px] w-[90%] rounded-xl p-4 border-2 border-slate-600 text-sm lg:text-lg' placeholder='Description'></input>
+        
         <button onClick={()=>handleSubmit()}
-        className='h-[50px] w-[100px] rounded-xl p-2 bg-blue-400 text-xl cursor-pointer'>Submit</button>
+        className='h-[50px] w-[100px] rounded-xl p-2 bg-blue-400 cursor-pointer text-sm lg:text-lg'>Submit</button>
         </div>
     </div>
     </div>
